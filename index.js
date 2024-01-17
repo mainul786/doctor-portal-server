71f0e45b9bfc88a625ac0c0be6ddb8b8de716c6f
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-01-17 21:37:43 +0530

    data-base connect to server

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0vnziom.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    
+
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', async(req, res)=>{
+    res.send('data send')
+})
+
+app.listen(port, ()=>{
+    console.log('data send successfully')
+})
