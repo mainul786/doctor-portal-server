b40894c4334dd1496f89298aa37d92c22f8c633f
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-02-05 08:58:00 +0530

    Payment Method intregate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
 app.use(cors());
@@ -117,6 +117,25 @@ async function run() {
       const result = await bookingsCollection.insertOne(bookings);
       res.send(result);
     })
+
+
+    app.post('/create-payment-intent', async(req, res)=>{
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+      
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency:'inr',
+        amount:amount,
+        "payment_method_type":[
+          "card"
+        ]
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
 
     app.get('/jwt', async (req, res) => {
       const email = req.query.email;
